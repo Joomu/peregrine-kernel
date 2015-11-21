db25659463183c01eb390adb17cb683ef0015a14
Joomu <Joomu@users.noreply.github.com>
2015-11-21 08:36:50 -0500

    Added build script

---
+++ build_kernel.sh
@@ -0,0 +1,8 @@
+echo -e "Building kernel\n"
+export PATH=$PATH:../toolchains/linaro_4.9.4/bin/
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-cortex_a7-linux-gnueabihf-
+
+make peregrine_defconfig
+make -j5
